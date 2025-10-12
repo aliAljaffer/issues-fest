7457bbc1e420c70aba664565afe5860d10486f4c
Ali Aljaffer <ahjfr@icloud.com>
2025-10-12 20:14:45 +0300

    feat: add Database class skeleton

---
+++ src/java/Database.java
@@ -0,0 +1,11 @@
+
+// Database connection class
+public class Database {
+    private String connectionString;
+    
+    public Database(String host, int port) {
+        this.connectionString = host + ":" + port;
+    }
+    
+    // TODO: Implement connection logic
+}
