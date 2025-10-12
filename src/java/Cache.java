91514bbf3b09397e1013bb82cb08eda51685b679
Ali Aljaffer <ahjfr@icloud.com>
2025-10-12 20:14:45 +0300

    feat: implement basic cache

---
+++ src/java/Cache.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+// Simple cache implementaton
+public class Cache {
+    private HashMap<String, Object> store;
+    
+    public Cache() {
+        this.store = new HashMap<>();
+    }
+    
+    // BUG: No TTL (time to live) support
+    public void put(String key, Object value) {
+        store.put(key, value);
+    }
+    
+    public Object get(String key) {
+        return store.get(key);
+    }
+}
